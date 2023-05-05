b06bf23d6087b88125060d5f61c5a5088a4c0716
Emirlan Dogdurbaev <jigit38@gmail.com>
2023-05-05 21:06:14 +0600

    fixed errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter as Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login/Login";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" index element={<Login />} />
-    </Routes>
+      <Routes>
+        <Route path="/" index element={<Login />} />
+      </Routes>
   );
 };
 
