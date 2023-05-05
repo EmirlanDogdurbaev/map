887a18b5ebf93081ce6ed7687c5df6983476958a
Emirlan Dogdurbaev <jigit38@gmail.com>
2023-05-05 21:12:44 +0600

    test

--- src/components/reducer/authReducer.js
+++ src/components/reducer/authReducer.js
@@ -1,21 +1,20 @@
 // authReducer.js
 
 const initialState = {
-      isAuthenticated: false,
-      user: null
-    };
-    
-    const authReducer = (state = initialState, action) => {
-      switch (action.type) {
-        case 'SET_CURRENT_USER':
-          return {
-            isAuthenticated: action.payload ? true : false,
-            user: action.payload
-          };
-        default:
-          return state;
-      }
-    };
-    
-    export default authReducer;
-    
+  isAuthenticated: false,
+  user: null,
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "SET_CURRENT_USER":
+      return {
+        isAuthenticated: action.payload ? true : false,
+        user: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
