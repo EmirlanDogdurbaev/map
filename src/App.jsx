b5c847bbf156aecd3a324da321a48165e45c59c8
Emirlan Dogdurbaev <jigit38@gmail.com>
2023-05-05 22:42:37 +0600

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Login from "./components/Login/Login";
+// import { logDOM } from "@testing-library/react";
+// import axios from "axios";
+// import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+// import { Route, Routes } from "react-router-dom";
+// import Login from "./components/Login/Login";
+import LoginForm from "./components/Login/LoginForm";
+import Profile from "./components/Profile/Profile";
 
 const App = () => {
+  const token = useSelector((state) => state.auth.token);
+
   return (
-      <Routes>
-        <Route path="/" index element={<Login />} />
-      </Routes>
+    <div>
+      {token ? <Profile /> : <LoginForm />}
+    </div>
+  
   );
 };
 
--- src/components/reducer/authReducer.js
+++ src/components/reducer/authReducer.js
@@ -1,16 +1,45 @@
+// // authReducer.js
+
+// const initialState = {
+//   isAuthenticated: false,
+//   user: null,
+// };
+
+// const authReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case "SET_CURRENT_USER":
+//       return {
+//         isAuthenticated: action.payload ? true : false,
+//         user: action.payload,
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+// export default authReducer;
+
+
 // authReducer.js
 
 const initialState = {
-  isAuthenticated: false,
-  user: null,
+  token: null,
+  error: null
 };
 
 const authReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "SET_CURRENT_USER":
+    case 'SET_CURRENT_USER':
       return {
-        isAuthenticated: action.payload ? true : false,
-        user: action.payload,
+        ...state,
+        token: action.payload,
+        error: null
+      };
+    case 'LOGIN_ERROR':
+      return {
+        ...state,
+        token: null,
+        error: action.payload
       };
     default:
       return state;
